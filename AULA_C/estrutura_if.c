69cfd65df60ec941414bce164e0727120079d7b2
MmorptEAD <202405186508@alunos.estacio.br>
2025-05-27 15:45:20 +0000

    estruturas de if e else

---
+++ AULA_C/estrutura_if.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    /* se a idade for 18 ou mais , o programa imprime 'Voce é maior de idade 
+    no nosso exemplo , 20 é maior que 18 , então essa mensagem sera exibida 
+    */
+
+    int idade = 20 ;
+
+    if (idade >= 18)
+        printf("Você é maior de idade \n");
+        printf("Fora IF");
+}
